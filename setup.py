37c3c69dea696bb1f49ea2ec80ba10695f0ad633
svabra <brayan@brayan.com>
2025-05-07 01:03:46 +0200

    add the confluent-kafka files

--- setup.py
+++ setup.py
@@ -18,18 +18,4 @@ setup(
         "httpx>=0.27.0",
         "pyyaml>=6.0.1",
     ]
-    # packages=['tksessentials'],
-    # package_dir={'tksessentials':'src'}
-    # packages=find_packages(),
-    # version='0.1.0',
-    # description='The library describes the most common models used in trading systems.',
-    # author='Brayan Svan',
-    # license='MIT',
-    # install_requires=['sqlmodel'],
-    # setup_requires=['pytest-runner'],
-    # tests_require=['pytest==4.4.1'],
-    # test_suite='tests',
-    # cmdclass={
-    #     'build': ProduceAvroSchemas,
-    # },
 )
